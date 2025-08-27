914d4840d05297863eb1dde040774de6cedc232d
Khale <derbasskhaled1@gmail.com>
2025-08-27 18:12:46 +0300

    Push wheelzy assesment

--- Wheelzy.App/Models/Car.cs
+++ Wheelzy.App/Models/Car.cs
@@ -7,9 +7,7 @@ public class Car
     public int MakeId { get; set; }
     public int ModelId { get; set; }
     public int? SubModelId { get; set; }
-    public string ZipCode { get; set; } = string.Empty;
-   // public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
-    
+    public string ZipCode { get; set; } = string.Empty;    
     public virtual Make Make { get; set; } = null!;
     public virtual Model Model { get; set; } = null!;
     public virtual SubModel? SubModel { get; set; }
